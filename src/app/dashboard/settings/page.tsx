d20b517dd55c90390f458f3ba3ccbd203ed97dc5
sendz <sendzation@gmail.com>
2024-09-10 12:36:38 +0700

    Update profile page

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,8 +1,63 @@
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { PropsWithChildren } from "react";
+"use server"
 
-export default function DashboardSettingsPage({ children }: Readonly<PropsWithChildren>) {
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { createClient } from "../../../../utils/supabase/server";
+import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+
+export default async function DashboardSettingsPage() {
+    const supabase = createClient();
+    const { data: { user } } = await supabase.auth.getUser();
+
+    if (!user) {
+        redirect("/login");
+    }
+
+    const { data: profile, error } = await supabase
+        .from("profiles")
+        .select("*")
+        .eq("id", user.id)
+        .single();
+
+    if (error) {
+        console.error(error);
+    }
+
+    const updateProfile = async (formData: FormData) => {
+        "use server"
+    
+        const supabase = createClient();
+        const { data, error } = await supabase
+            .from("profiles")
+            .update({
+                first_name: formData.get("first_name"),
+                last_name: formData.get("last_name"),
+                email: formData.get("email"),
+                phone_number: formData.get("phone_number"),
+                address: formData.get("address"),
+                emergency_contact: formData.get("emergency_contact"),
+                blood_type: formData.get("blood_type"),
+                allergies: formData.get("allergies"),
+                profession: formData.get("profession"),
+                education: formData.get("education"),
+                birth_date: formData.get("birth_date") || null,
+                gender: formData.get("gender"),
+            })
+            .eq("id", formData.get("id"))
+            .select()
+            .single();
+    
+        if (error) {
+            console.error(error);
+        }
+    
+        return data;
+    }
+
     return (
         <div>
             <Card>
@@ -17,7 +72,37 @@ export default function DashboardSettingsPage({ children }: Readonly<PropsWithCh
                         </TabsList>
                         <TabsContent value="profile">
                             <div>
-                                <h3>Profile</h3>
+                                <h3 className="text-lg font-medium pb-4">Profile Settings</h3>
+                                <div className="grid gap-4">
+                                    <form action={updateProfile} className="flex flex-col gap-2">
+                                        <input type="hidden" name="id" value={profile?.id} />
+                                        <Input name="first_name" label="First Name" defaultValue={profile?.first_name} />
+                                        <Input name="last_name" label="Last Name" defaultValue={profile?.last_name} />
+                                        <Input name="email" label="Email" defaultValue={profile?.email} />
+                                        <Input name="phone_number" label="Phone Number" defaultValue={profile?.phone_number} />
+                                        <Input name="address" label="Address" defaultValue={profile?.address} />
+                                        <Input name="emergency_contact" label="Emergency Contact" defaultValue={profile?.emergency_contact} />
+                                        <Input name="blood_type" label="Blood Type" defaultValue={profile?.blood_type} />
+                                        <Input name="allergies" label="Allergies" defaultValue={profile?.allergies} />
+                                        <Input name="profession" label="Profession" defaultValue={profile?.profession} />
+                                        <Input name="education" label="Education" defaultValue={profile?.education} />
+                                        <Input name="birth_date" label="Birth Date" defaultValue={profile?.birth_date} />
+                                        <Label>Gender</Label>
+                                        <Select name="gender">
+                                            <SelectTrigger>
+                                                <SelectValue placeholder="Select Gender" />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                <SelectGroup>
+                                                <SelectItem value="male">Male</SelectItem>
+                                                <SelectItem value="female">Female</SelectItem>
+                                                <SelectItem value="other">Other</SelectItem>
+                                                </SelectGroup>
+                                            </SelectContent>
+                                        </Select>
+                                        <Button className="mt-4" type="submit">Save</Button>
+                                    </form>
+                                </div>
                             </div>
                         </TabsContent>
                         <TabsContent value="family">
